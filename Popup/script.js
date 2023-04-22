8c600e30f387b0fee6d973c4d11b770df7e47aad
madeupgalaxy <madeupgalaxy@users.noreply.github.com>
2023-04-22 22:29:57 +0530

    h

--- Popup/script.js
+++ Popup/script.js
@@ -1,6 +1,8 @@
-<script>
-function myFunction(){
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-</script>
+document.querySelector("#open-popup").addEventListener("click", function(){
+     document.body.classList.add("active-popup");
+});
+
+
+document.querySelector("popup .close-btn").addEventListener("click", function(){
+  document.body.classList.remove("active-popup");
+});
