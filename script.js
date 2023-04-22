fc54476f74f8dd04f102964c8ef6c6558887def4
Advaita <vedanta3000@gmail.com>
2023-04-22 23:04:59 +0530

    added charts

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ $('#home').scrollIntoView(true, {
     inline: 'center'
 });
 
+
+   
+google.charts.load('current', {packages: ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Element', 'Density', { role: 'style' }],
+    ['Copper', 8.94, '#b87333'],            // RGB value
+    ['Silver', 10.49, 'silver'],            // English color name
+    ['Gold', 19.30, 'gold'],
+  
+  ['Platinum', 21.45, 'color: #e5e4e2' ], // CSS-style declaration
+  ]);
+  var chart = new google.visualization.ColumnChart(document.getElementById('daily-chart'));
+      chart.draw(data, {
+        backgroundColor: 'transparent'
+      });
+}
+
 // Ambient music
 
 let now_playing = document.querySelector(".now-playing");
